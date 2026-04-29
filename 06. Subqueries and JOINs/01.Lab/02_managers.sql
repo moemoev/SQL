5d72018f4515f014815d6e95893a10b85c0d6534
Dimo <dimo.dimow@tu-dortmund.de>
2026-04-29 08:38:42 +0200

    refactor: improve and add alternative solutions to exercises

--- 06. Subqueries and JOINs/01.Lab/02_managers.sql
+++ 06. Subqueries and JOINs/01.Lab/02_managers.sql
@@ -12,3 +12,26 @@ ON
 ORDER BY
 	e.employee_id
 LIMIT 5;
+
+
+
+-------------------
+-- If managers could be detected by having "Manager" in job_title, but maybe that can't be relied on so checking whether
+-- employee id is manager_id of each department ssems to be the approach
+-------------------
+SELECT 
+	E.employee_id,
+	CONCAT_WS(' ', E.first_name,E.last_name) AS full_name,
+	E.department_id,
+	D.name AS department_name
+FROM 
+	employees AS E
+JOIN
+	departments AS D
+USING
+	(department_id)
+WHERE 
+	job_title LIKE '%Manager%'
+ORDER BY
+	E.employee_id ASC
+LIMIT 5; 
